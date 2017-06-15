8c181a943c5353f0bc984ebfba0c050532663bac
Alexandr Makhin <amakhin@gmail.com>
2017-06-15 20:19:46 +0200

    'commit'

---
+++ Spring.WebUI/src/app/Shared/jsonHeader.service.ts
@@ -0,0 +1,23 @@
+﻿import { Injectable } from '@angular/core';
+import { Headers } from '@angular/http';
+
+@Injectable()
+export class JsonHeaderService {
+
+    constructor() { }
+    // for requesting unsecured data using json
+    jsonHeaders() {
+      const header = new Headers();
+        header.append('Content-Type', 'application/json');
+        header.append('Accept', 'application/json');
+        return header;
+    }
+
+    // for requesting unsecured data using form post
+    contentHeaders() {
+      const header = new Headers();
+        header.append('Content-Type', 'application/x-www-form-urlencoded');
+        header.append('Accept', 'application/json');
+        return header;
+    }
+}
