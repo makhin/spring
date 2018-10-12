6f1bed8f928e7141bc989d05d9ab585603de5eee
Alexandr Makhin <amakhin@gmail.com>
2018-10-12 14:15:45 +0200

    starter

--- spring.react/src/index.tsx
+++ spring.react/src/index.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const rootElement = document.getElementById("root");
+
+const store = createStore<StoreState>(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript',
+});
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <Hello />
+    </Provider>,
+    rootElement
 );
+
 registerServiceWorker();
