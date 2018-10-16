46a46b231d933497e1cf9e93a953a9ae49d5d99d
Alexandr Makhin <amakhin@gmail.com>
2018-10-16 13:23:53 +0200

    starter

--- spring.react/src/index.tsx
+++ spring.react/src/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { enthusiasm } from './reducers/index';
-import { StoreState } from './types/index';
-import { EnthusiasmAction } from './actions/index';
+import { Provider } from 'mobx-react';
+import { createBrowserHistory } from 'history';
+import { TodoModel } from './models/TodoModel';
+import { createStores } from './stores/createStore';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
@@ -19,14 +17,19 @@ import {AppRouter} from "./components/Router";
 
 const rootElement = document.getElementById("root");
 
-const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
-    enthusiasmLevel: 1,
-    languageName: 'TypeScript',
-});
+// default fixtures for TodoStore
+const defaultTodos = [
+    new TodoModel('Use Mobx'),
+    new TodoModel('Use React', true)
+];
+
+// prepare MobX stores
+const history = createBrowserHistory();
+const rootStore = createStores(history, defaultTodos);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>,
+<Provider {...rootStore}>
+            <AppRouter />
+        </Provider>,
     rootElement
 );
