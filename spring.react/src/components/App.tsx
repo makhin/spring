0e21127fd94adbbd8e58cb756df4281952e8aa72
Alexandr Makhin <amakhin@gmail.com>
2018-10-21 17:04:11 +0200

    starter

--- spring.react/src/components/App.tsx
+++ spring.react/src/components/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { Root } from '../containers/Root/Root';
-import { TodoApp } from '../containers/TodoApp';
+import {Root} from '../containers/Root/Root';
+import {TodoApp} from '../containers/TodoApp/TodoApp';
 import {Route, Router, Switch} from "react-router";
 
 export const App = ({history}) => (
