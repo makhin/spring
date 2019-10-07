caa79869592b43a43ca559c2a1dcb3c48f3684d1
Alexandr Makhin <amakhin@hotmail.com>
2019-10-07 21:39:49 +0200

    update to 3.0

--- Spring.WebApi/Program.cs
+++ Spring.WebApi/Program.cs
@@ -1,5 +1,6 @@
-﻿using System.IO;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
 
 namespace Spring.WebApi
 {
@@ -7,15 +8,14 @@ namespace Spring.WebApi
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- Spring.WebApi/Startup.cs
+++ Spring.WebApi/Startup.cs
@@ -3,45 +3,31 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.IO;
-using System.Linq;
 using System.Reflection;
 using AutoMapper;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json.Serialization;
 using Spring.DbContext.DbContext;
-using Spring.DbContext.Models;
 using Spring.Repositories;
 using Spring.Services;
-using Spring.WebApi.Filters;
-using Serilog;
 using Spring.Dto;
 
 namespace Spring.WebApi
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOpenApiDocument();
-            //services.AddSwaggerDocument();
+            services.AddControllers();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
@@ -65,113 +51,29 @@ namespace Spring.WebApi
             services.AddTransient<ILookupService, LookupService>();
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
-            services.AddTransient<IUserService, UserService>();            
-/*            
-            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-                {
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequiredLength = 6;
-                })
-                .AddEntityFrameworkStores<SpringDbContext>()
-                .AddDefaultTokenProviders();
-*/            
-            services.AddApplicationInsightsTelemetry(Configuration);
-
-            services.AddMvc(
-                config => { config.Filters.Add(typeof(SpringExceptionFilter)); }
-            ).SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddJsonOptions(opts =>
-            {
-                // Force Camel Case to JSON
-                opts.SerializerSettings.ContractResolver = new DefaultContractResolver
-                {
-                    NamingStrategy = new CamelCaseNamingStrategy()
-                };
-            });
-/*                                           
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("Manage Accounts", policy => policy.RequireRole("admin"));
-                options.AddPolicy("Access Resources", policy => policy.RequireRole("admin", "user"));
-            });
-*/
+            //services.AddApplicationInsightsTelemetry(Configuration);
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
-            /*            
-                        services.AddIdentityServer(options =>
-                            {
-                                options.IssuerUri = Configuration["IssuerUri"];
-                            })
-                            .AddDeveloperSigningCredential()
-                            .AddInMemoryPersistedGrants()
-                            .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                            .AddInMemoryApiResources(Config.GetApiResources())
-                            .AddInMemoryClients(Config.GetClients())
-                            .AddAspNetIdentity<ApplicationUser>();
-
-                        services.AddAuthentication(options =>
-                        {
-                            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                        }).AddJwtBearer(o =>
-                        {
-                            o.Authority = Configuration["Authority"];
-                            o.Audience = "WebAPI";
-                            o.RequireHttpsMetadata = false;
-                        });
-            */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            var serilog = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .Enrich.FromLogContext();
-
-            serilog
-                .WriteTo.File(
-                    outputTemplate:
-                    "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message}{NewLine}{Exception}{NewLine}",
-                    path: @"identityserver4_log.txt");
-
-            loggerFactory
-                .AddSerilog(serilog.CreateLogger());
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            var appRoutes = new[] {
-                "/api/",
-                "/connect/"
-            };
+            app.UseHttpsRedirection();
 
-            app.Use(async (context, next) =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                await next();
-
-                if (context.Response.StatusCode == 404 &&
-                    !Path.HasExtension(context.Request.Path.Value) &&
-                    !appRoutes.Contains(context.Request.Path.Value))
-                {
-                    context.Request.Path = "/index.html";
-                    await next();
-                }
+                endpoints.MapControllers();
             });
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseMvc();
-
-            app.UseSwagger(); // serve OpenAPI/Swagger documents
-            app.UseSwaggerUi3(); // serve Swagger UI
-//            app.UseReDoc(); // serve ReDoc UI
-                            //            app.UseIdentityServer();
         }
     }
 }
